9bc20e67773877e14d01f44edbf325eaeaf143cd
User <user@Users-MacBook-Pro.local>
2025-07-09 10:57:02 -0700

    fix: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+import { Button, buttonSizeProps, buttonVariantProps } from '@brandonfajardo/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const toggle = () => {
+    document.body.classList.toggle('dark');
+    if (document.body.style.backgroundColor === 'black') {
+      document.body.style.backgroundColor = 'white'
+    } else {
+      document.body.style.backgroundColor = 'black'
+    }
+  }
+
+  return (
+    <>
+    <Button className='mb-[50px]' onClick={toggle}>Toggle light/dark</Button>
+    <div className='flex flex-col gap-5'>
+          <div className='flex flex-row gap-5'>
+      <Button size={buttonSizeProps.XXSMALL} variant={buttonVariantProps.DESTRUCTIVE}>Destructive</Button>
+      <Button size={buttonSizeProps.XXSMALL} variant={buttonVariantProps.GHOST}>Ghost</Button>
+      <Button size={buttonSizeProps.XXSMALL} variant={buttonVariantProps.OUTLINE}>Outline</Button>
+      <Button size={buttonSizeProps.XXSMALL} variant={buttonVariantProps.SECONDARY}>Secondary</Button>
+          </div>
+          
+                    <div className='flex flex-row gap-5'>
+            <Button size={buttonSizeProps.XSMALL} variant={buttonVariantProps.DEFAULT}>Default</Button>
+      <Button size={buttonSizeProps.XSMALL} variant={buttonVariantProps.DESTRUCTIVE}>Destructive</Button>
+      <Button size={buttonSizeProps.XSMALL} variant={buttonVariantProps.GHOST}>Ghost</Button>
+      <Button size={buttonSizeProps.XSMALL} variant={buttonVariantProps.OUTLINE}>Outline</Button>
+      <Button size={buttonSizeProps.XSMALL} variant={buttonVariantProps.SECONDARY}>Secondary</Button>
+      </div>
+
+             <div className='flex flex-row gap-5'>
+            <Button size={buttonSizeProps.SMALL} variant={buttonVariantProps.DEFAULT}>Default</Button>
+      <Button size={buttonSizeProps.SMALL} variant={buttonVariantProps.DESTRUCTIVE}>Destructive</Button>
+      <Button size={buttonSizeProps.SMALL} variant={buttonVariantProps.GHOST}>Ghost</Button>
+      <Button size={buttonSizeProps.SMALL} variant={buttonVariantProps.OUTLINE}>Outline</Button>
+      <Button size={buttonSizeProps.SMALL} variant={buttonVariantProps.SECONDARY}>Secondary</Button>
+      </div>
+
+             <div className='flex flex-row gap-5'>
+      <Button size={buttonSizeProps.LARGE} variant={buttonVariantProps.DEFAULT}>Default</Button>
+      <Button size={buttonSizeProps.LARGE} variant={buttonVariantProps.DESTRUCTIVE}>Destructive</Button>
+      <Button size={buttonSizeProps.LARGE} variant={buttonVariantProps.GHOST}>Ghost</Button>
+      <Button size={buttonSizeProps.LARGE} variant={buttonVariantProps.OUTLINE}>Outline</Button>
+      <Button size={buttonSizeProps.LARGE} variant={buttonVariantProps.SECONDARY}>Secondary</Button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
